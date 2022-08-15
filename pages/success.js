d1611e7ad43800fcbde4d4c8ebb348320564886c
awhitebird18 <aaron.whitebird@gmail.com>
2022-08-15 01:12:26 -0400

    corrected netlify issue

---
+++ pages/success.js
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+const Success = () => {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16 bg-red-400"></div>
+  );
+};
+
+export default Success;
